2371fdb97f6036556acf593e80fd8c5dc3e97d77
Dhanraj Rateria <dhanrajrateria@gmail.com>
2024-07-25 23:16:47 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, Response
+from flask_socketio import SocketIO
+from invisible_cloak import InvisibleCloak
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+cloak = InvisibleCloak()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen():
+    while True:
+        frame = cloak.process_frame()
+        if frame is not None:
+            _, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
